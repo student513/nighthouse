cbc02766c875a784f3d084bebd676bcc9b478987
johyeongjun <compa311@gmail.com>
2021-01-20 10:44:44 +0900

    child-process lhci 테스트 성공
    lighthouserc.js api 주석처리 해야함

--- server/index.js
+++ server/index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const spawn = require("child_process").exec;
 
 const db = require("./db");
 const urlRouter = require("./routes/url-router");
 const agenda = require("./scheduler");
+const { cwd } = require("process");
 
 const app = express();
 const apiPort = 3001;
@@ -20,5 +22,8 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api", urlRouter);
+
+const lhci = spawn("lhci autorun", []);
+lhci.stdout.on("data");
 
 app.listen(apiPort, () => console.log(`Server running on port ${apiPort}`));

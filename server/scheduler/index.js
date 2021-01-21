af82d8dc3c6e26b1ae582f4c388732d3d813dcea
johyeongjun <compa311@gmail.com>
2021-01-22 00:13:58 +0900

    리포트에 식별요소(filename)추가

--- server/scheduler/index.js
+++ server/scheduler/index.js
@@ -24,12 +24,18 @@ try {
         `${process.env.SERVER_API_URL}/urls`
       );
       const urls = [];
+      // urls: [{url, _id}]
       urlsDocuments.data.data.forEach((doc) => {
-        urls.push(doc.url);
+        urls.push({ url: doc.url, _id: doc._id });
       });
+
+      // {url, _id}마다 lhci collect커맨드 실행
       urls.forEach((url) => {
-        const lhci = exec(`lhci collect --url=${url}`);
-        lhci.stdout.on("data", function (message) {
+        // url마다 분석 실시
+        const lhciCollect = exec(
+          `lhci collect --url=${url.url} && lhci upload --target=filesystem --reportFilenamePattern=%%DATETIME%%_${url._id}.%%EXTENSION%% --outputDir=./reports `
+        );
+        lhciCollect.stdout.on("data", function (message) {
           console.log(message);
         });
       });
@@ -37,20 +43,24 @@ try {
 
     agenda.define("uploadReport", (job) => {
       console.log("start upload");
-      fs.readdir("./.lighthouseci", "utf8", (err, filenames) => {
+
+      // reports 폴더의 모든 파일에 대하여
+      fs.readdir("./reports", "utf8", (err, filenames) => {
         if (err) {
           console.log("File read failed:", err);
           return;
         }
+        // 해당파일의 확장자가 .json이면
         filenames.forEach((filename) => {
-          if (filename.includes("lhr-") && filename.includes(".json")) {
-            console.log(`./.lighthouseci/${filename}`);
+          if (filename.includes(".json") && filename !== "manifest.json") {
+            // 해당 파일에 대해 filename을 삽입하여 파싱
             fs.readFile(
-              `./.lighthouseci/${filename}`,
+              `./reports/${filename}`, // filename에 _id를 넣고 filename을 json에 삽입
               "utf8",
               (err, content) => {
-                const report = JSON.parse(content);
+s                const report = JSON.parse(content);
                 const parsedReport = {
+                  "-id": filename,
                   "speed-index": report["audits"]["speed-index"],
                   "total-blocking-time":
                     report["audits"]["total-blocking-time"],
@@ -76,7 +86,6 @@ try {
                   `${process.env.SERVER_API_URL}/report`,
                   parsedReport
                 );
-                console.log(parsedReport);
               }
             );
           }
@@ -86,8 +95,8 @@ try {
 
     (async () => {
       await agenda.start();
-      await agenda.every("2 minutes", "getAnalysis");
-      await agenda.every("2 minutes", "uploadReport");
+      await agenda.every("4 minutes", "getAnalysis");
+      await agenda.every("7 minutes", "uploadReport");
     })();
   });
 } catch {

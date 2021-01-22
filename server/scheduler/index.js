e1e60b92875620aec120b84dc04b3e92c58b5c23
johyeongjun <compa311@gmail.com>
2021-01-22 16:28:04 +0900

    콜백인자 done을 넘겨주기 위해 job재배치

--- server/scheduler/index.js
+++ server/scheduler/index.js
@@ -3,28 +3,114 @@ const job = require("./job.js");
 
 require("dotenv").config();
 
+const exportProfileId = (filename) => {
+  const tmp = filename.split("-");
+  const id = tmp[1].split(".");
+  return id[0];
+};
+
 const agenda = new Agenda({
   db: {
     address: process.env.DB_URL,
     options: { useNewUrlParser: true, useUnifiedTopology: true },
     collection: "jobs", // 어느 컬랙션에 job 정보를 관리할 것인가?
   },
-  processEvery: "10 seconds",
+  processEvery: "30 seconds",
 });
 
 try {
   agenda.on("ready", async () => {
     console.log("Success agenda connecting");
 
-    agenda.define("getAnalysis", job.getAnalysis()); // ;세미콜론 없으면 에러
-    agenda.define("uploadReport", job.uploadReport()); // ;세미콜론 없으면 에러
-    agenda.define("resetReport", job.resetReport());
+    agenda.define("getAnalysis", async (done) => {
+      console.log("start Analysis");
+      const urlsDocuments = await axios.get(
+        `${process.env.SERVER_API_URL}/urls`
+      );
+      const urls = [];
+      // urls: [{url, _id}]
+      urlsDocuments.data.data.forEach((doc) => {
+        urls.push({ url: doc.url, _id: doc._id });
+      });
+
+      // {url, _id}마다 lhci collect커맨드 실행
+      urls.forEach((url) => {
+        // url마다 분석 실시
+        const lhciCollect = exec(
+          `lhci collect --url=${url.url} && lhci upload --target=filesystem --reportFilenamePattern=%%DATETIME%%-${url._id}.%%EXTENSION%% --outputDir=./reports `
+        );
+        lhciCollect.stdout.on("data", function (message) {
+          console.log(message);
+        });
+      });
+      console.log("finish Analysis", Date());
+      done();
+    }); // ;세미콜론 없으면 에러
+
+
+    agenda.define("uploadReport", (done) => {
+      console.log("start upload");
+
+      // reports 폴더의 모든 파일에 대하여
+      fs.readdir("./reports", "utf8", (err, filenames) => {
+        if (err) {
+          console.log("File read failed:", err);
+          return;
+        }
+        // 해당파일의 확장자가 .json이면
+        filenames.forEach((filename) => {
+          if (filename.includes(".json") && filename !== "manifest.json") {
+            // 해당 파일에 대해 filename을 삽입하여 파싱
+            fs.readFile(
+              `./reports/${filename}`, // filename에 _id를 넣고 filename을 json에 삽입
+              "utf8",
+              (err, content) => {
+                const report = JSON.parse(content);
+                const parsedReport = {
+                  "profileId": exportProfileId(filename),
+                  "speedIndex": report["audits"]["speed-index"],
+                  "totalBlockingTime": report["audits"]["total-blocking-time"],
+                  "firstContentfulPaint":
+                    report["audits"]["first-contentful-paint"],
+                  "timeToInteractive": report["audits"]["interactive"],
+                  "largeContentfulPaint":
+                    report["audits"]["largest-contentful-paint"],
+                  "cumulativeLayoutShift":
+                    report["audits"]["cumulative-layout-shift"],
+                  "unminifiedJavascript":
+                    report["audits"]["unminified-javascript"],
+                  "serverResponseTime": report["audits"]["server-response-time"],
+                  "performance": report["categories"]["performance"]["score"],
+                  "accessibility": report["categories"]["accessibility"]["score"],
+                  "bestPractices":
+                    report["categories"]["best-practices"]["score"],
+                  "seo": report["categories"]["seo"]["score"],
+                };
+                axios.post(
+                  `${process.env.SERVER_API_URL}/report`,
+                  parsedReport
+                );
+              }
+            );
+          }
+        });
+      });
+      console.log("finish upload", Date());
+      done();
+    }); // ;세미콜론 없으면 에러
+    agenda.define("resetReport", (done) => {
+      const reset = exec(`rm -rf ./reports && mkdir reports`);
+      reset.stdout.on("data", function (message) {
+        console.log(message);
+      });
+      done();
+    });
 
     (async () => {
       await agenda.start();
-      await agenda.every("4 minutes", "getAnalysis");
-      await agenda.every("7 minutes", "uploadReport");
-      await agenda.every("1 minutes", "resetReport");
+      await agenda.every("3 minutes", "getAnalysis");
+      await agenda.every("15 minutes", "uploadReport");
+      await agenda.every("7 minutes", "resetReport");
     })();
   });
 } catch {

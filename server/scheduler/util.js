c559d1c11fd10011989828f7c5acbe145cc4365f
johyeongjun <compa311@gmail.com>
2021-01-25 16:16:55 +0900

    코드리뷰 반영
    dotenv 단일화,express method chaining, 서버실행 스크립트 추가, job 예외처리, exportProfileId 모듈화

---
+++ server/scheduler/util.js
@@ -0,0 +1,7 @@
+const exportProfileId = (filename) => {
+  const idAndExtension = filename.split("-");
+  const id = idAndExtension[1].split(".");
+  return id[0];
+};
+
+module.exports = { exportProfileId };

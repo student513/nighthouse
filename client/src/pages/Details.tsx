e048d00c61cf032005cf0db9a17d0578fb959ee9
johyeongjun <compa311@gmail.com>
2021-01-29 16:54:55 +0900

    차트 다중 생성 및 삭제기능 추가

--- client/src/pages/Details.tsx
+++ client/src/pages/Details.tsx
@@ -1,5 +1,6 @@
-import { createContext, useEffect, useState } from "react"
+import { useEffect, useState } from "react"
 import { RouteComponentProps } from "react-router-dom"
+import uuid from "uuid"
 
 import { getReports } from "../api"
 import { ReportData } from "../interfaces/ReportType"
@@ -10,11 +11,17 @@ type Props = {
   profileId: string
 }
 
-export const setSelectedReportDateContext = createContext(null)
-
 const Details = ({ match }: RouteComponentProps<Props>) => {
   const [reportList, setReportList] = useState<ReportData[]>([])
-  const [selectedReportDate, setSelectedReportDate] = useState<Date>(new Date())
+  const [chartArray, setChartArray] = useState<string[]>([])
+
+  const addReportChart = () => {
+    setChartArray((oldArray) => [...oldArray, uuid.v4()])
+  }
+  const removeReportChart = (id: string) => {
+    const array = chartArray.filter((chart) => chart !== id)
+    setChartArray(array)
+  }
 
   const getReportsByProfileId = async (id: string) => {
     const reports = await getReports(id)
@@ -27,7 +34,13 @@ const Details = ({ match }: RouteComponentProps<Props>) => {
 
   return (
     <div>
-      <ReportChart reportList={reportList} />
+      <button onClick={addReportChart}>차트 추가</button>
+      {chartArray.map((chartId) => (
+        <div key={chartId}>
+          <ReportChart reportList={reportList} />
+          <button onClick={() => removeReportChart(chartId)}>차트 삭제</button>
+        </div>
+      ))}
     </div>
   )
 }

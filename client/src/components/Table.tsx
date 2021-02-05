c1837da7b535da16ca2fdd01c833d6122dbfec45
johyeongjun <compa311@gmail.com>
2021-02-05 21:23:38 +0900

    parsedReportCollection 정적 타이핑: any 사용하지 않기

--- client/src/components/Table.tsx
+++ client/src/components/Table.tsx
@@ -27,12 +27,57 @@ const Table = ({ reportList }: Props) => {
       alert(ReportErrorMessage.NOT_SELECT_PERIOD)
       return
     }
-    const parsedReportCollection = periodParsedReportList.reduce((acc, cur) => {
-      ;(Object.keys(periodParsedReportList[0]) as Array<keyof ReportData>).forEach((key: keyof ReportData) => {
-        acc[key] ? acc[key].push(cur[key]) : (acc[key] = [cur[key]])
-      })
-      return acc
-    }, {} as any)
+    const parsedReportCollection = periodParsedReportList.reduce(
+      (acc, cur) => {
+        acc["speedIndex"] ? acc["speedIndex"].push(cur.speedIndex) : (acc["speedIndex"] = [cur["speedIndex"]])
+        acc["totalBlockingTime"]
+          ? acc["totalBlockingTime"].push(cur.totalBlockingTime)
+          : (acc["totalBlockingTime"] = [cur.totalBlockingTime])
+        acc["firstContentfulPaint"]
+          ? acc["firstContentfulPaint"].push(cur.firstContentfulPaint)
+          : (acc["firstContentfulPaint"] = [cur.firstContentfulPaint])
+        acc["timeToInteractive"]
+          ? acc["timeToInteractive"].push(cur.timeToInteractive)
+          : (acc["timeToInteractive"] = [cur.timeToInteractive])
+        acc["largestContentfulPaint"]
+          ? acc["largestContentfulPaint"].push(cur.largestContentfulPaint)
+          : (acc["largestContentfulPaint"] = [cur.largestContentfulPaint])
+        acc["cumulativeLayoutShift"]
+          ? acc["cumulativeLayoutShift"].push(cur.cumulativeLayoutShift)
+          : (acc["cumulativeLayoutShift"] = [cur.cumulativeLayoutShift])
+        acc["unminifiedJavascript"]
+          ? acc["unminifiedJavascript"].push(cur.unminifiedJavascript)
+          : (acc["unminifiedJavascript"] = [cur.unminifiedJavascript])
+        acc["serverResponseTime"]
+          ? acc["serverResponseTime"].push(cur.serverResponseTime)
+          : (acc["serverResponseTime"] = [cur.serverResponseTime])
+        acc["performance"] ? acc["performance"].push(cur.performance) : (acc["performance"] = [cur.performance])
+        acc["accessibility"]
+          ? acc["accessibility"].push(cur.accessibility)
+          : (acc["accessibility"] = [cur.accessibility])
+
+        acc["bestPractices"]
+          ? acc["bestPractices"].push(cur.bestPractices)
+          : (acc["bestPractices"] = [cur.bestPractices])
+        acc["seo"] ? acc["seo"].push(cur.seo) : (acc["seo"] = [cur.seo])
+
+        return acc
+      },
+      {
+        speedIndex: [] as number[],
+        totalBlockingTime: [] as number[],
+        firstContentfulPaint: [] as number[],
+        timeToInteractive: [] as number[],
+        largestContentfulPaint: [] as number[],
+        cumulativeLayoutShift: [] as number[],
+        unminifiedJavascript: [] as number[],
+        serverResponseTime: [] as number[],
+        performance: [] as number[],
+        accessibility: [] as number[],
+        bestPractices: [] as number[],
+        seo: [] as number[],
+      }
+    )
     setTableParsedValues(parsedReportCollection)
   }
 
@@ -42,7 +87,6 @@ const Table = ({ reportList }: Props) => {
       .map((key) => {
         return getRepresentativeValues(reportListCollection[key], key)
       })
-    valueList.pop() // __v 제거
     setTableValues(valueList)
   }
 

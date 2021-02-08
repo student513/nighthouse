5aa3122f79ff81d552552c10b5a36ab1d7c332fd
johyeongjun <compa311@gmail.com>
2021-02-08 14:45:27 +0900

    TimeParser 함수명 camel case로 수정

--- client/src/utils/TimeParser.ts
+++ client/src/utils/TimeParser.ts
@@ -1,9 +1,9 @@
-const ReportDateParser = (time: string) => {
+const reportDateParser = (time: string) => {
   const Time = new Date(time)
   return `${Time.getFullYear()}년 ${Time.getMonth()}월 ${Time.getDate()}일 ${Time.getHours()}시`
 }
 
-const CardDateParser = (time: string) => {
+const cardDateParser = (time: string) => {
   const Time = new Date(time)
   const parsedMonth = Time.getMonth() < 10 ? `0${Time.getMonth()}` : `${Time.getMonth()}`
   const parsedDate = Time.getDate() < 10 ? `0${Time.getDate()}` : `${Time.getDate()}`
@@ -13,4 +13,4 @@ const CardDateParser = (time: string) => {
   return `${Time.getFullYear()}.${parsedMonth}.${parsedDate} ${parsedHours}:${parsedMinutes}`
 }
 
-export { ReportDateParser, CardDateParser }
+export { reportDateParser, cardDateParser }
